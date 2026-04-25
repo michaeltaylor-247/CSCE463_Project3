d350d994ed69b8e75db5c55ddbe243a25e00ac5e
Evan <ehpo@tamu.edu>
2026-04-25 15:35:12 -0500

    Full producer and consumer setup for vibration finished based off the documentation setup

--- producers/vibration/VibProducer.h
+++ producers/vibration/VibProducer.h
@@ -1,5 +1,6 @@
 #ifndef VIBPRODUCER_H
 #define VIBPRODUCER_H
+#include <librdkafka/rdkafkacpp.h> 
 
 #include "../../utility/Event.h"
 
@@ -9,10 +10,13 @@
 class VibProducer {
 private:
     std::mt19937 gen;                           // rng engine
-    std::uniform_int_distribution<> dist;       // distribution
+    RdKafka::Producer* producer; // Kafka producer instance
+    std::string topic = "factory.sensors.vibration"; // Kafka topic for vibration data
 
 public:
-    VibProducer();
+    VibProducer(const std::string& brokers); //Constructor to initialize the Kafka producer with the given broker list
+    ~VibProducer(); //Destructor to clean up the Kafka producer instance
+    void send(const Event& event); //Method to send an Event object to the Kafka topic
     Event produce();
 };
 
